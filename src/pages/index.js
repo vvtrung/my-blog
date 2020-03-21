c08d58860a75c2e15b9168f5c464fadbed88249c
trungvv-1622 <vo.van.trung@sun-asterisk.com>
2020-03-21 17:05:25 +0700

    apply theme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,72 +1,130 @@
 import React from 'react';
-import { Link, graphql } from 'gatsby';
+import styled from 'styled-components';
+import tw from 'tailwind.macro';
+import { Parallax } from 'react-spring/renderprops-addons.cjs';
 
-import Bio from '../components/bio';
-import Layout from '../components/layout';
-import SEO from '../components/seo';
-import { rhythm } from '../utils/typography';
+import Layout from '../components/LandingPage/Layout';
+import ProjectCard from '../components/ProjectCard';
+import { Title, BigTitle, SubTitle, Inner } from '../components/common';
+import Hero from '../components/LandingPage/Hero';
+import Projects from '../components/LandingPage/Projects';
+import About from '../components/LandingPage/About';
+import Contact from '../components/LandingPage/Contact';
+import avatar from '../styles/images/avatar.jpg';
 
-const BlogIndex = ({ data, location }) => {
-  const siteTitle = data.site.siteMetadata.title;
-  const posts = data.allMarkdownRemark.edges;
-
-  return (
-    <Layout location={location} title={siteTitle}>
-      <SEO title="All posts" />
-      <Bio />
-      {posts.map(({ node }) => {
-        const title = node.frontmatter.title || node.fields.slug;
-        return (
-          <article key={node.fields.slug}>
-            <header>
-              <h3
-                style={{
-                  marginBottom: rhythm(1 / 4),
-                }}
-              >
-                <Link style={{ boxShadow: 'none' }} to={node.fields.slug}>
-                  {title}
-                </Link>
-              </h3>
-              <small>{node.frontmatter.date}</small>
-            </header>
-            <section>
-              <p
-                dangerouslySetInnerHTML={{
-                  __html: node.frontmatter.description || node.excerpt,
-                }}
-              />
-            </section>
-          </article>
-        );
-      })}
-    </Layout>
-  );
-};
-
-export default BlogIndex;
-
-export const pageQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      edges {
-        node {
-          excerpt
-          fields {
-            slug
-          }
-          frontmatter {
-            date(formatString: "MMMM DD, YYYY")
-            title
-            description
-          }
-        }
-      }
-    }
+const ProjectsWrapper = styled.div`
+  ${tw`flex flex-wrap justify-between mt-8`};
+  display: grid;
+  grid-gap: 4rem;
+  grid-template-columns: repeat(2, 1fr);
+  @media (max-width: 1200px) {
+    grid-gap: 3rem;
+  }
+  @media (max-width: 900px) {
+    grid-template-columns: 1fr;
+    grid-gap: 2rem;
   }
 `;
+
+const AboutHero = styled.div`
+  ${tw`flex flex-col lg:flex-row items-center mt-8`};
+`;
+
+const Avatar = styled.img`
+  ${tw`rounded-full w-32 xl:w-48 shadow-lg h-auto m-0`};
+`;
+
+const AboutSub = styled.span`
+  ${tw`text-white pt-12 lg:pt-0 lg:pl-12 text-xl lg:text-2xl xl:text-3xl`};
+`;
+
+const AboutDesc = styled.p`
+  ${tw`text-grey-light text-lg md:text-l lg:text-xl font-sans pt-10 md:pt-16 text-justify`};
+`;
+
+const ContactText = styled.p`
+  ${tw`text-grey-light font-sans text-xl md:text-2xl lg:text-3xl`};
+`;
+
+const Footer = styled.footer`
+  ${tw`text-center text-grey absolute pin-b p-6 font-sans text-md lg:text-lg`};
+`;
+
+export default () => (
+  <>
+    <Layout />
+    <Parallax pages={5}>
+      <Hero offset={0}>
+        <BigTitle>
+          Hello, <br /> I'm Vo Van Trung.
+        </BigTitle>
+        <SubTitle>I'm creating noice web experiences for the next generation of consumer-facing companies.</SubTitle>
+      </Hero>
+      <Projects offset={1}>
+        <Title>Projects</Title>
+        <ProjectsWrapper>
+          <ProjectCard
+            title="Freiheit"
+            link="https://www.behance.net/gallery/58937147/Freiheit"
+            bg="linear-gradient(to right, #D4145A 0%, #FBB03B 100%)"
+          >
+            This project is my entry to Adobe's #ChallengeYourPerspective contest.
+          </ProjectCard>
+          <ProjectCard
+            title="Harry Potter"
+            link="https://www.behance.net/gallery/52915793/Harry-Potter"
+            bg="linear-gradient(to right, #662D8C 0%, #ED1E79 100%)"
+          >
+            I entered the DOCMA 2017 award with this Harry Potter inspired image.
+          </ProjectCard>
+          <ProjectCard
+            title="Tomb Raider"
+            link="https://www.behance.net/gallery/43907099/Tomb-Raider"
+            bg="linear-gradient(to right, #009245 0%, #FCEE21 100%)"
+          >
+            Recreation of a Tomb Raider Wallpaper (Fan Art)
+          </ProjectCard>
+          <ProjectCard
+            title="Eagle"
+            link="https://www.behance.net/gallery/38068151/Eagle"
+            bg="linear-gradient(to right, #D585FF 0%, #00FFEE 100%)"
+          >
+            A fantasy image manipulation relocating the habitat of wild animals.
+          </ProjectCard>
+        </ProjectsWrapper>
+      </Projects>
+      <About offset={3}>
+        <Title>About</Title>
+        <AboutHero>
+          <Avatar src={avatar} alt="John Doe" />
+          <AboutSub>
+            The English language can not fully capture the depth and complexity of my thoughts. So I'm incorporating
+            Emoji into my speech to better express myself. Winky face.
+          </AboutSub>
+        </AboutHero>
+        <AboutDesc>
+          You know the way you feel when you see a picture of two otters holding hands? That's how you're gonna feel
+          every day. My mother cried the day I was born because she knew she’d never be prettier than me. You should
+          make me your campaign manager. I was born for politics. I have great hair and I love lying. Captain? The kids
+          want to know where Paulie the Pigeon is. I told them he got sucked up into an airplane engine, is that all
+          right?
+        </AboutDesc>
+      </About>
+      <Contact offset={4}>
+        <Inner>
+          <Title>Get in touch</Title>
+          <ContactText>
+            Say <a href="mailto:plizNoSp4m@domain.tld">Hi</a> or find me on other platforms:{' '}
+            <a href="https://dribbble.com/LekoArts">Dribbble</a> &{' '}
+            <a href="https://www.instagram.com/lekoarts.de/">Instagram</a>
+          </ContactText>
+        </Inner>
+        <Footer>
+          &copy; 2019 by Gatsby Starter Portfolio Cara.{' '}
+          <a href="https://github.com/LekoArts/gatsby-starter-portfolio-cara">Github Repository</a>. Made by{' '}
+          <a href="https://www.lekoarts.de">LekoArts</a>.
+        </Footer>
+      </Contact>
+    </Parallax>
+  </>
+);
